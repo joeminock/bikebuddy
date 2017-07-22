37c63b7ed52931acfc174a579f8e544239daab5c
Joe Minock <joe.minock@gmail.com>
2017-07-22 12:43:33 -0600

    Added Registrations Controller, Added sign up and sign in redirect actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
+  root 'pages#home'
+
   # PAGES - Static Content
   get 'about',   to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'terms',   to: 'pages#terms'
 
   devise_for :users
-
-  root 'pages#home'
 end
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
