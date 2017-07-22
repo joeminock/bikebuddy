aac9ef2042c3a337b19b7f6b673f2aded8d01305
Joe Minock <joe.minock@gmail.com>
2017-07-22 11:52:35 -0600

    Change Pages Index to Pages Home. Update Pages Controller. Set Root Route to Pages Home. Update Header. Add Sign Up and Sing In links to header

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'pages/index'
 
-  get 'pages/about'
-
-  get 'pages/contact'
-
-  get 'pages/terms'
+  # PAGES - Static Content
+  get 'about',   to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'terms',   to: 'pages#terms'
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'pages#home'
 end
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
