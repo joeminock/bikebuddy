8954b6494ca0c4cec3d6643d887b7374a8488c2c
Joe Minock <joe.minock@gmail.com>
2017-07-21 18:45:39 -0600

    Begin integration of Eastern Template

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'pages/terms'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
